3147ea266adc89b136f9b7dae5e095d8bf275b12
mzeybek <muzo583@gmail.com>
2020-06-17 22:22:34 +0300

    Update datanormalization.R

--- Code/datanormalization.R
+++ Code/datanormalization.R
@@ -8,10 +8,11 @@ df <- rnorm(10, mean = 40000, sd=2000)
 df
 hist(df) # plot data
 
-mx <- 255 # maximum value of normalization
 
-normalize <- function(df, mx, int=TRUE, plot=TRUE){
-  norm <- (df - min(df)) * mx / (max(df) - min(df))
+normalize <- function(df, mn, mx, int=TRUE, plot=TRUE){
+  range <- max(df) - min(df)
+  norm <- (df - min(df))/ (range)
+  norm <- mn + as.integer((mx-mn)*norm)
   if (int==TRUE) {
     norm <- as.integer(norm)
   }
@@ -24,4 +25,7 @@ normalize <- function(df, mx, int=TRUE, plot=TRUE){
   par(mfrow=c(1,1))    # set the plotting area into a 1*2 array
   
 }
-normalize(df, mx, TRUE, TRUE)
+
+mx <- 200 # maximum value of normalization
+mn <- 10
+normalize(df, mn, mx, TRUE, TRUE)
