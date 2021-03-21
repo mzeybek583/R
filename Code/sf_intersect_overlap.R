ef23d336a651dec195ec0572868c68c6a424ad00
mzeybek <muzo583@gmail.com>
2021-03-21 18:15:49 +0300

    Create sf_intersect_overlap.R

---
+++ Code/sf_intersect_overlap.R
@@ -0,0 +1,24 @@
+library(sf)
+
+## Loading required package: methods
+
+## Linking to GEOS 3.5.1, GDAL 2.1.2, proj.4 4.9.3
+
+pol = st_polygon(list(rbind(c(0,0), c(1,0), c(1,1), c(0,1), c(0,0))))
+b = st_sfc(pol, pol + c(.9, .2), pol + c(.2, .9))
+#par(mar = rep(0, 4))
+plot(b,col = sf.colors(3, categorical = TRUE), axes = TRUE)
+
+i = st_intersection(st_sf(b))
+#par(mar = rep(0, 9))
+cl = sf.colors(3, categorical = TRUE)
+plot(b)
+plot(i[i$n.overlaps == 2,], col = cl[1], add = TRUE, axes=TRUE)
+plot(i[i$n.overlaps == 3,], col = cl[2], add = TRUE, axes=TRUE)
+
+(i = st_intersection(b))
+
+d = st_difference(b)
+plot(d, col = cl, axes=TRUE)
+st_overlaps(i)
+st_overlaps(d)
