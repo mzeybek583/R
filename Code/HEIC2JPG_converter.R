b3121ab00298e90e14c6933878e5fd815a9b1c5f
mzeybek <muzo583@gmail.com>
2022-08-16 21:33:22 +0300

    Update HEIC2JPG_converter.R

--- Code/HEIC2JPG_converter.R
+++ Code/HEIC2JPG_converter.R
@@ -1,18 +1,17 @@
 
 library(magick)
 
-setwd("E:/DR_Sonrasi_Projeler/TurkLiDAR/iphone_road/drive-download-20220816T180142Z-001/")
-lst.file <- grep(list.files(path="E:/DR_Sonrasi_Projeler/TurkLiDAR/iphone_road/drive-download-20220816T180142Z-001/"), 
+setwd("set/your/image/path/as/working/directory")
+lst.file <- grep(list.files(path="set/your/image/path/"), 
                  pattern="*.HEIC", invert = FALSE, value = TRUE)
 
-if (!dir.exists("JPG")) {dir.create("JPG")}
+if (!dir.exists("JPG")) {dir.create("JPG")} # create new folder to export JPEG files
 
 for (ls in 1:length(lst.file)) {
-  #ls <- 1
-  path <- lst.file[ls]
+   path <- lst.file[ls]
   f.name <- tools::file_path_sans_ext(path)
   img <- image_read(path = path)
-  img_jpg <- image_convert(img, "jpeg")
+  img_jpg <- image_convert(img, "jpeg") # you can change jpeg to various format. Please see help file of Magick package
   #print(img_jpg)
   image_write(img_jpg, path = paste0("JPG/",f.name,".jpeg"))
 }
