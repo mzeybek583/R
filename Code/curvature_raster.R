5eed17c2b390f6feb16fecc6754110a91b0c0e8c
mzeybek <muzo583@gmail.com>
2024-10-21 01:45:17 +0300

    Update curvature_raster.R

--- Code/curvature_raster.R
+++ Code/curvature_raster.R
@@ -1,13 +1,21 @@
-# NOT RUN {
-library(raster)
+# Load necessary libraries
+if (!require("terra")) install.packages("terra", dependencies = TRUE)
+if (!require("spatialEco")) install.packages("spatialEco", dependencies = TRUE)
+library(terra)
 library(spatialEco)
-data(elev)
-elev <- projectRaster(elev, crs="+proj=robin +datum=WGS84", 
-                      res=1000, method='bilinear')
-curvature(elev, type="planform")
-mcnab.crv <- curvature(elev, type="mcnab")
-plot(mcnab.crv, main="McNab's curvature") 
-# }
-# NOT RUN {
 
-# }
+# Sample elevation data (you can replace this with your own dataset)
+elev <- rast(system.file("ex/elev.tif", package = "terra"))
+
+# Project the raster to Robinson projection with specified resolution
+elev_proj <- project(elev, "+proj=robin +datum=WGS84", res = 1000, method = "bilinear")
+
+# Calculate curvature (planform and McNab)
+planform_curvature <- curvature(elev_proj, type = "planform")
+mcnab_curvature <- curvature(elev_proj, type = "mcnab")
+
+# Plot the results
+plot(mcnab_curvature, main = "McNab's Curvature (Projected)")
+
+# Optional: Save the raster
+writeRaster(mcnab_curvature, "mcnab_curvature.tif", overwrite = TRUE)
