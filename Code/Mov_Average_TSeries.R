8ffd3513643bf447abe280d4637a7198944a424d
mzeybek <muzo583@gmail.com>
2024-10-21 01:29:59 +0300

    Update Mov_Average_TSeries.R

--- Code/Mov_Average_TSeries.R
+++ Code/Mov_Average_TSeries.R
@@ -1,22 +1,27 @@
-## Reference: https://gist.github.com/dsparks/4176957
+if (!require("zoo")) install.packages("zoo", dependencies = TRUE)
+library(zoo)
 
-
-library(zoo) 
-
+# Load the FTSE index data
 ftseIndex <- EuStockMarkets[, 4]
-plot(ftseIndex, col = "GRAY")
 
-# Calculate 10-day rolling mean, quickly:
-smoothIndex <- rollmean(x = ftseIndex,  # original series
-                        k = 30,  # width of the rolling window
-                        fill = NA)  # Pads head and/or tail with NA
-length(ftseIndex) == length(smoothIndex)
-lines(smoothIndex, col = "RED")
+# Plot the original FTSE index data
+plot(ftseIndex, col = "gray", main = "FTSE Index with Rolling Means", ylab = "Index Value", xlab = "Time")
 
-# If there are NA values in the original series, you'll need rollapply()
+# Calculate a 30-day rolling mean
+smoothIndex <- rollmean(x = ftseIndex, k = 30, fill = NA)
+
+# Add the rolling mean line to the plot
+lines(smoothIndex, col = "red")
+
+# Introduce NA values to the FTSE index data
 ftseIndex[c(40, 90, 300)] <- NA
-smoothIndex2 <- rollapply(data = ftseIndex,  # original series
-                          width = 90,  # width of the rolling window
-                          FUN = mean, na.rm = T,  # Any arbitrary function
-                          fill = NA)  # Padding
-lines(smoothIndex2, col = "GREEN")
+
+# Calculate a 90-day rolling mean with NA handling
+smoothIndex2 <- rollapply(data = ftseIndex, width = 90, FUN = mean, na.rm = TRUE, fill = NA)
+
+# Add the second rolling mean line to the plot
+lines(smoothIndex2, col = "green")
+
+# Add a legend to distinguish the lines
+legend("topright", legend = c("Original FTSE Index", "30-day Rolling Mean", "90-day Rolling Mean with NA Handling"),
+       col = c("gray", "red", "green"), lty = 1)
